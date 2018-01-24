8fae310fd1b440b956f00737510767a6a3ef61a6
keshav0016 <keshav.b@westagilelabs.com>
2018-01-24 14:42:30 +0530

    first commit

--- require/notes.js
+++ require/notes.js
@@ -1,1 +1,11 @@
-console.log("starting notes.js")
+console.log("starting notes.js");
+
+module.exports.age=25;
+module.exports.addNote = () => {
+	console.log('addNote');
+	return 'New note';
+}
+
+module.exports.add = (a,b) => {
+	return (a+b);
+}
--- require/require.js
+++ require/require.js
@@ -4,5 +4,8 @@ const notes = require('./notes.js');
 
 var user = os.userInfo();
 
+var res = notes.addNote();
+console.log(res);
+console.log('Result : ', notes.add(2,-5));
 console.log(user);
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
