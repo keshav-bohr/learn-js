fb3bcda9bff4d71f0302c30b824612edeac869a9
keshav0016 <keshav.b@westagilelabs.com>
2018-01-25 12:10:48 +0530

    first commit

--- require/require.js
+++ require/require.js
@@ -12,4 +12,4 @@ console.log('Result : ', notes.add(2,-5));
 console.log(user);
 fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`+'\n');
 console.log(_.isString('hello'));
-console.log(_.uniq([1,2,1,2]));
+console.log(_.uniq([1,2,1,2,3]));
