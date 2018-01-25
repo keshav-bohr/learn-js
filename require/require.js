180444d7504e6bf6b906e735141e12cb40796eb3
keshav0016 <keshav.b@westagilelabs.com>
2018-01-25 12:04:02 +0530

    first commit

--- require/require.js
+++ require/require.js
@@ -1,6 +1,8 @@
+
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ =require('lodash');
 
 var user = os.userInfo();
 
@@ -9,3 +11,5 @@ console.log(res);
 console.log('Result : ', notes.add(2,-5));
 console.log(user);
 fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`+'\n');
+console.log(_.isString('hello'));
+console.log(_.uniq([1,2,1,2]));
